ee5c248bd1e1cfb2521a49fc48bcc4aa27a6a396
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-04-04 13:10:08 +0200

    x

---
+++ model.js
@@ -0,0 +1,58 @@
+
+let model = {
+    app: {
+        language: ['Norwegian',],
+        darkmode: false,
+        login: false,
+    },
+    input: {
+        userActivity: {
+            searchbar: ["javaScript", "CSS",],
+            UncheckedProjects: [
+                {projectID:"11" ,projectName:"bruh", projectPhoto:"bruh.png", projectCode:["Python",],},
+            ],
+        },
+    },
+    data: {
+        
+        users: [
+            {
+            id: 0, 
+            username: "BjarnePirat",
+            eMail: "yarr@getAcademy.com",
+            password: "treasure123",
+            repassword: "treasure123",
+            firstName: "Bjarne",
+            lastName: "Pirat",
+            profilePicure: 'url',
+            phoneNr: 12345678,
+            age: 60,
+            bio: "i like trains",
+            country: "Karibia",
+            github: 'url',
+            linkedIn: 'url',
+            friendsID: [],
+            }
+        ],
+        projects: {
+            name: "hehexD",
+            description: 'bla bla',
+            author: 1,
+            files: [
+                {
+                    id: 0,
+                    picture: 'url',
+                    content: 'code',
+                    percenteachlanguage: {},
+                }
+            ],
+            comments: [
+                {
+                    id: 1,
+                    comment: 'blah'
+                }
+            ],
+        }
+    },
+}
+
