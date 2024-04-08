5e6a81e230f87d73916b4880255670ead853c390
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-04-08 14:16:52 +0200

    x

--- View/homescreen.js
+++ View/homescreen.js
@@ -2,7 +2,7 @@ homescreen()
 function homescreen(){
     app.innerHTML = /*HTML*/`
     <div id="userlist"><div>${genuserlist()}</div></div>
-    <div id="logo">logo</div>
+    <img id="logo" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1af7f6b3-4ef8-461f-a732-af3d73f70c8e/dgp01pk-7f41f8c1-89d7-4fac-b066-406f1eaed9e2.jpg/v1/fill/w_1920,h_1098,q_75,strp/the_open_sea_by_thecosmicdreamer_dgp01pk-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFhZjdmNmIzLTRlZjgtNDYxZi1hNzMyLWFmM2Q3M2Y3MGM4ZVwvZGdwMDFway03ZjQxZjhjMS04OWQ3LTRmYWMtYjA2Ni00MDZmMWVhZWQ5ZTIuanBnIiwiaGVpZ2h0IjoiPD0xMDk4Iiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvMWFmN2Y2YjMtNGVmOC00NjFmLWE3MzItYWYzZDczZjcwYzhlXC90aGVjb3NtaWNkcmVhbWVyLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.L9mSOCMMc74YL1kKx_KdB2xu0ZgehVrq62NTeRCSWSI"/>
     <input id="searchbar">
     <button id="loginbtn" onclick="logInscreen()">login</button>
     <div id="wordcloud"><div>${genwordcloud()}</div></div>
