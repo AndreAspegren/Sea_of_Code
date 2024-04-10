a65882c3fa29557e856c5bdbb2f26e2df8c13a8b
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-04-10 09:54:03 +0200

    x

--- View/registerView.js
+++ View/registerView.js
@@ -5,8 +5,8 @@ function registerUser(){
     <button id="darkmode" onclick="darkmode()">Dark mode</button>
     </div>
     <div id="registerDiv" class="registerDiv">
-    <h2>Registrer bruker</h2>
     <div id="registeruser">
+    <h2>Registrer bruker</h2>
     <input type="text" onchange="inputChange('eMail', this.value)" placeholder="Email" required />
     <input type="text" onchange="inputChange('username', this.value)" placeholder="Brukernavn" required/>
     <input type="password" id="passwordOne" onchange="inputChange('passwordOne', this.value)" placeholder="Passord" required/>
@@ -19,12 +19,12 @@ function registerUser(){
     <input type="text" onchange="inputChange('github', this.value)" placeholder="Github link (frivillig)" />
     <input type="text" onchange="inputChange('linkedIn', this.value)" placeholder="LinkedIn (frivillig)" />
     <input type="file" id="fileInput" onchange="fileChange(event)">
-    </div>
     <textarea id="bio" onchange="inputChange('bio', this.value)" placeholder="Bio"></textarea>
     Last opp profilbilde:
     <img id="profilePicture"/>
+    <button style="width: 10vh;" onclick="userRegister()">Registrer bruker</button>
+    </div>
 
-    <button onclick="userRegister()">Registrer bruker</button>
     </div>
     `
 ;
