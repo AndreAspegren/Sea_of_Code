e1f93a1e15b730257a77873793ed60d0008e9003
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-04-10 13:25:46 +0200

    x

---
+++ View/projectpage.js
@@ -0,0 +1,9 @@
+function projectpage(){
+    app.innerHTML = /*HTML*/`
+    <div>prosjektinfo</div>
+    <div>prosjektinfo</div>
+    <div>prosjektinfo</div>
+    <button onclick="darkmode()" id="darkmode">darkmode</button>
+    <img id="logo" onclick="updateview('homescreen')" src="img/logo.jpg"/>
+    `
+}
