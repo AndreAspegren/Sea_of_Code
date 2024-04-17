c25d5645f203e01650ec1c3f0fc1ab77fc0839ae
RobinRinnanSommerstad <robin1998@gmail.com>
2024-04-17 12:25:45 +0200

    x

--- View/profilescreen.js
+++ View/profilescreen.js
@@ -2,132 +2,83 @@ function profileScreen(key) {
     if (key != undefined) model.app.currentprofile = key
     const user = model.data.users[key == undefined ? model.app.currentprofile : key]
     app.innerHTML = /*HTML*/`
-    <div id="profileScreen">
-    <div class="container">
-        <div class="profile-header">
-            <div class="profile-img">
-                <img src="url" width=""
-                alt="">
-            </div>
-            <div class="profile-nav-info">
-                <h3 class="user-name">${user.username}</h3>
-            </div>
-            <div class="profile-option">
-                <div class="notification">
-                    <i class="fa fa-bell">🔔</i>
-                    <span class="alert-message">1</span>
-                </div>
-            </div>
+<div id="profileScreen">
+  <div class="container">
+    <div class="profile-header">
+      <div class="profile-img">
+        <img src="url" width="" alt="">
+      </div>
+      <div class="profile-nav-info">
+        <h3 class="user-name">${user.username}</h3>
+      </div>
+      <div class="profile-option">
+        <div class="notification">
+          <i class="fa fa-bell">🔔</i>
+          <span class="alert-message">1</span>
         </div>
-        <div class="main-bd">
-            <div class="left-side">
-                <div class="profile-side">
-                    <p class="mobile-no"><i class="fa fa-phone">📞</i>${user.phoneNr}</p>
-                    <p class="user-mail"><i class="fa fa-envelope">📧</i>${user.eMail}</p>
-                    <div class="user-bio">
-                        <h3>Bio</h3>
-                        <p class="bio">${user.bio}</p>
-                    </div>
-                    <div class="profile-btn">
-                        ${model.app.loggedIn && model.app.userID != model.app.currentprofile ? `<button class="chatbtn" onclick="model.app.currentprofiletab = 'chat'; updateview()">
-                        <i class="fa fa-comment"></i> 🗨Chat 
-                        </button>` : ''}
-                        <button class="createbtn">
-                        <i class="fa fa-plus"></i> 	➕Create
-                        </button>
-                    </div>
-                    <br>
-                    <button onclick="logOff()">Log off</button>
-                    <div class="user-socialmedia">
-                        <p class="socialmedia"></p>
-                    </div>
-                </div>
-            </div>
-            <div class="right-side">
-                <div class="nav">
-                    <ul>
-                        <li onclick="model.app.currentprofiletab = 'friends'; updateview()" class="user-post active">Friends</li>
-                        <li onclick="model.app.currentprofiletab = 'uploads'; updateview()" class="user-uploads">Uploads</li>
-                        <li onclick="model.app.currentprofiletab = 'settings'; updateview()" class="user-setting">Settings</li>
-                    </ul>
-                </div>
-                <div class="profile-body">
-                    <div class="profile-posts tab">
-                        <h1>Your posts</h1>
-                        <p>No posts</p>
-                    </div>
-                    <div class="profile-uploads tab">
-                        <h1>Your uploads</h1>
-                        <p>No uploads</p>
-                    </div>
-                    <div class="profile-settings tab">
-                        <h1>Account setting</h1>
-                        <p>No settings</p>
-                    </div>
-                </div>
-            </div>
+      </div>
+    </div>
+    <div class="main-bd">
+      <div class="left-side">
+        <div class="profile-side">
+          <p class="mobile-no"><i class="fa fa-phone">📞</i>${user.phoneNr}</p>
+          <p class="user-mail"><i class="fa fa-envelope">📧</i>${user.eMail}</p>
+          <div class="user-bio">
+            <h3>Bio</h3>
+            <p class="bio">${user.bio}</p>
+          </div>
+          <div class="profile-btn">
+            ${model.app.loggedIn && model.app.userID != model.app.currentprofile ? `<button class="chatbtn" onclick="model.app.currentprofiletab = 'chat'; updateview()">
+              <i class="fa fa-comment"></i> 🗨Chat 
+            </button>` : ''}
+            <button class="createbtn">
+              <i class="fa fa-plus"></i> 	➕Create
+            </button>
+          </div>
+          <br>
+          <button onclick="logOff()">Log off</button>
+          <div class="user-socialmedia">
+            <p class="socialmedia"></p>
+          </div>
         </div>
+      </div>
+      <div class="right-side">
+        <div class="nav">
+          <ul>
+            <li class="user-post active">Friends</li>
+            <li class="user-uploads">Uploads</li>
+            <li class="user-setting">Settings</li>
+          </ul>
+        </div>
+        <div class="profile-body">
+          <div class="profile-posts tab active">
+            <h1>Your posts</h1>
+            <p>No posts</p>
+          </div>
+          <div class="profile-uploads tab">
+            <h1>Your uploads</h1>
+            <p>No uploads</p>
+          </div>
+          <div class="profile-settings tab">
+            <h1>Account setting</h1>
+            <p>No settings</p>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    <button onclick="darkmode()" id="darkmode">darkmode</button>
-    <img id="logo" onclick="model.app.currentprofiletab = null; updateview('homescreen')" src="img/logo.jpg"/>
-    <div>${!model.app.loggedIn ? '' : genfriendbtn(key, user)}</div>
-    <div id="${model.app.currentprofiletab ?? 'friends'}">
-    ${model.app.currentprofiletab == 'friends' ? genfriendlist(key == undefined ? model.app.currentprofile : key) : ''}
-    ${model.app.currentprofiletab == 'uploads' ? genuploads(key == undefined ? model.app.currentprofile : key) : ''}
-    ${model.app.currentprofiletab == 'settings' ? gensettings(key == undefined ? model.app.currentprofile : key) : ''}
-    ${model.app.currentprofiletab == 'chat' ? genchat(key == undefined ? model.app.currentprofile : key) + /*HTML*/`
+  </div>
+</div>
+<button onclick="darkmode()" id="darkmode">darkmode</button>
+<img id="logo" onclick="model.app.currentprofiletab = null; updateview('homescreen')" src="img/logo.jpg"/>
+<div>${!model.app.loggedIn ? '' : genfriendbtn(key, user)}</div>
+<div id="${model.app.currentprofiletab ?? 'friends'}">
+  ${model.app.currentprofiletab == 'friends' ? genfriendlist(key == undefined ? model.app.currentprofile : key) : ''}
+  ${model.app.currentprofiletab == 'uploads' ? genuploads(key == undefined ? model.app.currentprofile : key) : ''}
+  ${model.app.currentprofiletab == 'settings' ? gensettings(key == undefined ? model.app.currentprofile : key) : ''}
+  ${model.app.currentprofiletab == 'chat' ? genchat(key == undefined ? model.app.currentprofile : key) + /*HTML*/`
     <div id="msgbox">
-    <input oninput="model.input.userActivity.message = this.value">
-    <button onclick="sendmsg()">Send</button></div>  
-    ` : ''}
-    </div>
-    <button id="mutebtn" onclick="mutebtn()">Mute</button>
-`
-}
-
-function genfriendbtn(key, user) {
-    if (key != model.app.userID && !model.data.users[model.app.userID].friends.includes(key)) return `<button onclick="addfriend(${user.id})">Legg til venn</button>`
-    if (model.data.users[model.app.userID].friends.includes(key)) return '<div>Dere er venner</div>'
-    return ''
-}
-
-function genuploads(key) {
-    return 'hei'
-}
-
-function genchat() {
-    return model.data.messages
-        .filter(m => (m.from == 0 || m.from == 1) || (m.to == 0 || m.to == 1))
-        .map(m => {
-            let currentclass = m.from == model.app.userID ? 'rightmsg' : 'leftmsg'
-            return `<div id="${currentclass}">${m.content}</div>`
-        })
-        .join('');
-}
-
-function genfriendlist(key) {
-    let friends = ''
-    for (let i = 0; i < model.data.users[key].friends.length; i++) {
-        friends += /*HTML*/`
-        <div id="friendcards" onclick="updateview('profileScreen', ${key})">
-        <div>
-        <div>${model.data.users[key].username}</div>
-        <div>${model.data.users[key].projects.length} prosjekter</div>
-        <div>${model.data.users[key].friends.length} venner</div>
-        </div>
-        <img style="height: 6vh; width: auto" src="${model.data.users[key].profilePicure}"/>
-        </div>
-        `
-    }
-    return friends
-}
-
-function gensettings(key) {
-    return 'deg'
-}
-
-function addfriend(key) {
-    model.data.users[model.app.userID].friends.push(key)
-    updateview()
-}
+      <input oninput="model.input.userActivity.message = this.value">
+      <button onclick="sendmsg()">Send</button>
+    </div>` : ''}
+</div>
+<button id="mutebtn" onclick="mutebtn()">Mute</button>
--- controller/profileController.js
+++ controller/profileController.js
@@ -1,19 +1,23 @@
-$('.nav ul li').click(function () {
-  $(this).addClass("active").siblings().removeClass('active');
-})
+document.querySelectorAll('.nav ul li').forEach(function(tab, index) {
+  tab.addEventListener('click', function() {
+    // Add 'active' class to the clicked tab and remove it from siblings
+    tab.classList.add("active");
+    document.querySelectorAll('.nav ul li').forEach(function(sibling) {
+      if (sibling !== tab) {
+        sibling.classList.remove('active');
+      }
+    });
 
-
-
-const tabBtn = document.querySelectorAll('.nav ul li');
-const tab = document.querySelectorAll('tab');
-
-function tabs(panelIndex) {
-  tab.forEach(function (node) {
-    node.style.display = 'none';
+    // Hide all tabs and show the one with the corresponding index
+    document.querySelectorAll('.tab').forEach(function(tabContent, tabIndex) {
+      if (tabIndex === index) {
+        tabContent.style.display = 'block';
+      } else {
+        tabContent.style.display = 'none';
+      }
+    });
   });
-  tab[panelIndex].style.display = 'block';
-}
-
+});
 
 function logOff() {
   let currentUserId = model.app.userID;
@@ -26,31 +30,28 @@ function logOff() {
   updateview('homescreen');
 }
 
+let bio = document.querySelector('.bio');
 
+function bioText() {
+  bio.oldText = bio.innerText;
+  bio.innerText = bio.innerText.substring(0, 100) + "...";
+  bio.innerHTML += "&nbsp;" + `<span onclick='addLength()' id='see-more-bio'> See More </span>`;
+}
 
+bioText();
 
-// let bio = document.querySelector('.bio');
-
-// function bioText() {
-//   bio.oldText = bio.innerText;
-//   bio.innerText = bio.innerText.substring(0,100) + "...";
-//   bio.innerHTML += "&nbsp;" + `<span onclick='addLength()' id='see-more-bio'> See More </span>`;
-// }
-
-// bioText();
-
-// function addLength() {
-//   bio.innerHTML =bio.oldText;
-//   bio.innerHTML += "&nbsp;" + `<span onclick='bioText()' id='see-less-bio'> See Less </span>`;
-// }
+function addLength() {
+  bio.innerHTML = bio.oldText;
+  bio.innerHTML += "&nbsp;" + `<span onclick='bioText()' id='see-less-bio'> See Less </span>`;
+}
 
 function sendmsg(){
   model.data.messages.push({
-          from: model.app.userID, 
-          to: model.app.currentprofile, 
-          Datesent: new Date().toISOString().substr(0, 16).replace('T', ' '), 
-          content: model.input.userActivity.message,
-       },)
-       updateview()
+    from: model.app.userID, 
+    to: model.app.currentprofile, 
+    Datesent: new Date().toISOString().substr(0, 16).replace('T', ' '), 
+    content: model.input.userActivity.message,
+  });
+  updateview();
 }
 
