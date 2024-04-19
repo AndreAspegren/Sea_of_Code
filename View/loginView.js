2bde8efd646f9c07d7c65bfc0c85f522136a250b
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-04-19 13:12:36 +0200

    x

--- View/loginView.js
+++ View/loginView.js
@@ -1,22 +1,24 @@
-function logInscreen(){
+function logInscreen() {
     app.innerHTML = /*HTML*/`
-    <div id=mainDiv>
-<div style="grid-area: header"> 
+        <div id=mainDiv>
+<div id="buttons">
 <img id="logo" onclick="updateview('homescreen')" src="img/logo.jpg"/>
 <button id="mutebtn" onclick="mutebtn()">Mute</button>
 <button id="darkmode" onclick="darkmode()">Dark mode</button>
 </div>
 
-    <div id="loginscreen" class="content" style="grid-area: content">
+    <div id="loginscreen">
     <h2> Logg inn </h2>
     <input type="text" id="usernameInput"  placeholder="Brukernavn" required/>
     <input type="password" id="passwordInput"  placeholder="passord"  required/>
     <button onclick="login()">Logg inn</button>
     </div>
-    <div style="grid area: footer">
+
+    <div id="footer">
     Ikke registrert? Lag bruker her:
     <button id="register" onclick="registerUser()"> Registrer bruker </button>
     </div>
-    </div>
+        </div>
     `
-;}
+        ;
+}
