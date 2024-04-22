dba575cd97f2f07d0d9a8f6e75e9e37a7be69afd
RobinRinnanSommerstad <robin1998@gmail.com>
2024-04-22 13:13:55 +0200

    Fiksa homescreen btn, mutebte og darkmode

--- View/profilescreen.js
+++ View/profilescreen.js
@@ -5,10 +5,15 @@ function profileScreen(key) {
 <div id="profileScreen">
   <div class="container">
     <div class="profile-header">
+    <img id="logo" onclick="model.app.currentprofiletab = null; updateview('homescreen'); model.app.currentprofiletab = ''" src="https://cdn.pixabay.com/photo/2023/11/12/16/48/pirate-8383445_1280.jpg"/><div>${!model.app.loggedIn && key != model.app.userID ? '' : genfriendbtn(key, user)}</div>
       <div class="profile-img">
       <img src="${user.profilePicture}" width="" alt="">     </div>
       <div class="profile-nav-info">
         <h3 class="user-name">${user.username}</h3>
+      </div>
+      <div class= "darkMute">
+      <button onclick="darkmode()" id="darkmode">darkmode</button>
+      <button id="mutebtn" onclick="mutebtn()">Mute</button>
       </div>
       <div class="profile-option">
         <div class="notification">
