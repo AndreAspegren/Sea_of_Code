65d7498d1ab5cf05047d9a30498ade537071f7a8
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-04-22 10:44:33 +0200

    x

--- View/loginView.js
+++ View/loginView.js
@@ -1,24 +1,27 @@
 function logInscreen() {
     app.innerHTML = /*HTML*/`
-        <div id=mainDiv>
-<div id="buttons">
-<img id="logo" onclick="updateview('homescreen')" src="https://cdn.pixabay.com/photo/2023/11/12/16/48/pirate-8383445_1280.jpg"/>
-<button id="mutebtn" onclick="mutebtn()">Mute</button>
-<button id="darkmode" onclick="darkmode()">Dark mode</button>
-</div>
-
-    <div id="loginscreen">
-    <h2> Logg inn </h2>
-    <input type="text" id="usernameInput"  placeholder="Brukernavn" required/>
-    <input type="password" id="passwordInput"  placeholder="passord"  required/>
-    <button onclick="login()">Logg inn</button>
-    </div>
-
-    <div id="footer">
-    Ikke registrert? Lag bruker her:
-    <button id="register" onclick="registerUser()"> Registrer bruker </button>
-    </div>
+        <div id="allDiv">
+        <div id="logobtn">
+        <img id="logo" onclick="updateview('homescreen')" src="https://cdn.pixabay.com/photo/2023/11/12/16/48/pirate-8383445_1280.jpg"/>
         </div>
-    `
-        ;
+
+        <div id="buttons">
+                <button id="mutebtn" onclick="mutebtn()">Mute</button>
+                <button id="darkmode" onclick="darkmode()">Dark mode</button>
+            </div>
+            
+            <div id="loginscreen">
+                <h2> Logg inn </h2>
+                <input type="text" id="usernameInput" placeholder="Brukernavn" required/>
+                <input type="password" id="passwordInput" placeholder="Passord" required/>
+                <button onclick="login()">Logg inn</button>
+            </div>
+
+
+            <div id="footer">
+                Ikke registrert? Lag bruker her:
+                <button id="register" onclick="registerUser()"> Registrer bruker </button>
+            </div>
+        </div>
+    `;
 }
