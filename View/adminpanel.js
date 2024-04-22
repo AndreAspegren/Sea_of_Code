d9e94b144c33c00b591f6dac24bd95979660ad79
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-04-22 13:32:09 +0200

    x

--- View/adminpanel.js
+++ View/adminpanel.js
@@ -1,23 +1,24 @@
 function adminpanel() {
     app.innerHTML = /*HTML*/`
     <div id="admincontainer">
+    <div id="nonadminprojects">${adminprojects()}</div>
 
-    <div id="adminlogo">
-    <img  onclick="updateview('homescreen')" src="https://cdn.pixabay.com/photo/2023/11/12/16/48/pirate-8383445_1280.jpg"/>
-    </div>
+        <div id="adminlogo">
+            <img onclick="updateview('homescreen')" src="https://cdn.pixabay.com/photo/2023/11/12/16/48/pirate-8383445_1280.jpg"/>
+        </div>
 
-    <div id="adminbuttons">
-    <button onclick="darkmode()" id="darkmode">darkmode</button>
-    <button id="mutebtn" onclick="mutebtn()">Mute</button>
-    </div> 
-    
-    <div id="nonadminusers">${gennonadminlist()}</div>
-    
-    <div id="adminprojects">${adminprojects()}</div>
+        <div id="adminbuttons">
+            <button onclick="darkmode()" id="darkmode">darkmode</button>
+            <button id="mutebtn" onclick="mutebtn()">Mute</button>
+        </div> 
+        
+        <div id="nonadminusers">${gennonadminlist()}</div>
+        
 
     </div>
     `
 }
+
 
 function adminprojects() {
     let projects = ''
