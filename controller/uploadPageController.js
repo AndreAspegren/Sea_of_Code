4cced19f7b2d0d80c1e024216123d21a3df19f7c
Albert Filip <albert.filip30@yahoo.com>
2024-04-24 11:11:49 +0200

    x

--- controller/uploadPageController.js
+++ controller/uploadPageController.js
@@ -5,16 +5,8 @@
 
 function sendProjectInfo() { // download :o
     let currentUserId = model.app.userID;
-<<<<<<< HEAD
     pushProject();
 
-=======
-    if (model.data.adminpanel.users.includes(currentUserId)) {
-        pushProjectTrue();
-    } else {
-        pushProjectFalse();
-    }
->>>>>>> 86a257aea9472811b6063da239b262016b04ce53
 }
 
 function pushProject() {
@@ -43,35 +35,6 @@ function pushProject() {
 }
 
 
-<<<<<<< HEAD
-=======
-function pushProjectTrue() {
-    model.data.wordCloud[model.input.projects.language]++
-    model.input.projects.language = null
-    model.data.projects.push(
-        {
-            id: model.data.users[model.app.userID].projects.length - 1,
-            approved: true,
-            name: model.input.name,
-            lastUpdated: new Date().toISOString().substr(0, 16).replace('T', ' '),
-            dateCreated: new Date().toISOString().substr(0, 16).replace('T', ' '),
-            description: model.input.projects.description,
-            author: model.app.userID,
-            picture: model.input.projects.picture,
-            files: [{
-                id: model.app.loggedIn,
-                name: '',
-                picture: '',
-                content: '',
-                language: 'Javascript',
-                percentEachLanguage: {},
-            },],
-            comments: [],
-        },
-    )
-    updateview('homescreen')
-}
->>>>>>> 86a257aea9472811b6063da239b262016b04ce53
 //  model.data.friends: [
 // {
 //     1: 'Bjarne', 2: Per, friendsSince: 2020-05-05,
