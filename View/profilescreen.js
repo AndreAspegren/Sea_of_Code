06915509c3fdf0d3a82246ffad2f6a4d748979bf
RobinRinnanSommerstad <robin1998@gmail.com>
2024-04-26 14:49:14 +0200

    La til det Rebecka sa, og gjorde nav fra en class til en id

--- View/profilescreen.js
+++ View/profilescreen.js
@@ -50,7 +50,19 @@ function profileScreen(key) {
         </div>
       </div>
       <div class="right-side">
-        <div class="nav">
+        <div id="nav">
+          <script>
+              // Add active class to the current button (highlight it)
+            var header = document.getElementById("myDIV");
+            var btns = header.getElementsByClassName("btn");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function() {
+              var current = document.getElementsByClassName("active");
+              current[0].className = current[0].className.replace(" active", "");
+              this.className += " active";
+              });
+            }
+         </script>
           <ul>
             <li class="user-friends" onclick="model.app.currentprofiletab = 'friends'; tabs(1); updateview()">Venner</li>
             <li class="user-uploads" onclick="model.app.currentprofiletab = 'uploads'; tabs(2); updateview()">Opplastinger</li>
