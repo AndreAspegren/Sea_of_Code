d258b8ad9ca24145f6ec11151bab7984de2eaec8
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-04-29 13:31:46 +0200

    0

--- View/uploadPageView.js
+++ View/uploadPageView.js
@@ -21,6 +21,20 @@ function uploadPageView() {
     <div>
     <li>Upload files</li>
     <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
+    <input  type="file" id="fileUpload">
     </div>
 
     <div>
