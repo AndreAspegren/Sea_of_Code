8705301612bbb8e2ea18b6b0874d78b18e8efbfa
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-05-06 10:01:43 +0200

    x

--- View/loginView.js
+++ View/loginView.js
@@ -3,7 +3,6 @@ function logInscreen() {
     
         <div id="allDiv">
         <div id="logobtn">
-        <img id="logo" onclick="updateview('homescreen')" src="https://cdn.pixabay.com/photo/2023/11/12/16/48/pirate-8383445_1280.jpg"/>
         </div>
 
         <div id="buttons">
